a0668d62595c8e5cc50c00f6b26fb2c21ca0c145
Justin Wellesley-Smith <justin@synergifirst.com.au>
2014-09-29 16:55:10 +1000

    Added Campaigns and modified Subscribers
    Now includes subscribe and unsubscribe user...

--- src/Campaigns.php
+++ src/Campaigns.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Created by: gellu
- * Date: 13.09.2013 13:52
- * Modified: 25.09.2014 15:26 AEST By: Synergi
+ * Created by: Synergi
+ * Date: 25.09.2014 15:26 AEST
+ * 
  */
 
-$app->group('/lists', function() use ($app, $db) {
+$app->group('/campaigns', function() use ($app, $db) {
 
     $app->get('/get', function() use ($app, $db) {
 
-        if(!$app->request()->get('name'))
+        if(!$app->request()->get('id'))
         {
-            echo json_encode(array('status' => 'error', 'result' => 'Parameter [name] is missing'));
+            echo json_encode(array('status' => 'error', 'result' => 'Parameter [id] is missing'));
             $app->stop();
         }
 
-        $sth = $db->prepare('SELECT * FROM lists WHERE name LIKE :name');
-        $sth->execute(array('name' => '%' . $app->request()->get('name') .'%'));
+        $sth = $db->prepare('SELECT * FROM campaigns WHERE id = :id');
+        $sth->execute(array('id' => $app->request()->get('id')));
         $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 
         echo json_encode(array('status' => 'ok', 'result' => $res));
@@ -36,8 +36,8 @@ $app->group('/lists', function() use ($app, $db) {
         $sth->execute(array('app_key' => $app->request()->get('app_key')));
         $app = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-        //Get lists
-        $sth = $db->prepare('SELECT * FROM lists WHERE app = :id');
+        //Get Campaigns
+        $sth = $db->prepare('SELECT * FROM campaigns WHERE app = :id');
         $sth->execute(array('id' => $app[0]['id']));
         $res = $sth->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode(array('status' => 'ok', 'result' => $res));
